8b5695f887033852eb65d66d5d854c0885685478
colorizerde <colorizerdev@gmail.com>
2025-03-11 03:59:45 +0200

    Fix merge conflict

--- router/chatRoutes.js
+++ router/chatRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const multer = require("multer");
-const ChatController = require("../controllers/ChatController");
+const ChatController = require("../controllers/chatController");
 
 const router = express.Router();
 
